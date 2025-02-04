5155218b5f9fc7fd510c9eb3a6f34311d29641a8
Danya7007 <danafickolinec@gmail.com>
2025-02-05 00:11:49 +0200

    change file

--- script.js
+++ script.js
@@ -9,3 +9,11 @@ function searchWeb() {
         document.getElementById("search-result").style.color = "red";
     }
 }
+window.addEventListener('scroll', function() {
+    const text = document.querySelector('.animated-text');
+    const rect = text.getBoundingClientRect();
+    
+    if (rect.top <= window.innerHeight && rect.bottom >= 0) {
+        text.classList.add('visible');
+    }
+});
